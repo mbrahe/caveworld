d1f57a2b8762e81fec6c4e542cee81ec9b0a56b0
Jonathan Basalyga <bajona1@ad.umbc.edu>
2020-02-20 15:01:13 -0500

    Hi

---
+++ Assets/FootController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootController : MonoBehaviour
+{
+    public HeadController head;
+    public float stepRadius = 2;
+    public int raycastFineness = 4;
+
+    public void Step(Vector2 direction)
+    {
+        Vector2 target = direction.normalized * stepRadius * head.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude;
+        Vector2 origin = target + Vector2.up * .5f;
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, 10);
+        Debug.Log(hit.collider);
+
+        if (hit.collider != null && Mathf.Abs(Vector2.Dot(hit.normal.normalized, Vector2.up)) > .6)
+        {
+            Vector3 newPos = new Vector3(hit.point.x, hit.point.y, transform.position.z);
+            transform.position = newPos;
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/HeadController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadController : MonoBehaviour
+{
+    public FootController[] feet;
+    public float height = 1;
+    public float stiffness = 1;
+    public float leanForce = 2;
+    public float drag = .5f;
+    public float balanceTolerance = 1.5f;
+    public float stepLength = 1;
+
+    Vector2 inputDirection;
+
+    float stepTimer;
+    Rigidbody2D rb;
+
+    Vector2 meanFootPosition {
+        get
+        {
+            Vector2 meanPosition = Vector2.zero;
+            foreach (FootController foot in feet)
+            {
+                meanPosition += (Vector2)foot.gameObject.transform.position;
+            }
+
+            return meanPosition / feet.Length;
+        }
+    }
+
+    Vector2 target
+    {
+        get
+        {
+            return meanFootPosition + height * Vector2.up;
+        }
+    }
+
+    FootController farthestFoot
+    {
+        get
+        {
+            FootController farthest = null;
+            float farthestDistance = 0;
+            foreach (FootController foot in feet)
+            {
+                float distance = (foot.transform.position - transform.position).magnitude;
+                if (distance > farthestDistance)
+                {
+                    farthest = foot;
+                    farthestDistance = distance;
+                }
+            }
+
+            return farthest;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        foreach (FootController foot in feet)
+        {
+            foot.head = GetComponent<HeadController>();
+        }
+    }
+
+    private void Update()
+    {
+        inputDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        stepTimer -= Time.deltaTime;
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce((target - (Vector2) transform.position) * stiffness);
+        rb.AddForce(leanForce * inputDirection);
+        rb.AddForce(-rb.velocity * drag);
+
+        Vector2 targetSeperation = ((Vector2)transform.position) - target;
+
+        if (targetSeperation.magnitude > balanceTolerance && stepTimer <= 0)
+        {
+            farthestFoot.Step(inputDirection);
+            stepTimer = stepLength;
+        }
+    }
+}
